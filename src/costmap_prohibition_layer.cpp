5dced55cf1938d4523e7f857b6273cebf479b0ef
Stephan Kurzawe <stephan.kurzawe@tu-dortmund.de>
2016-10-27 13:43:04 +0200

    Licence text added to the source code

--- src/costmap_prohibition_layer.cpp
+++ src/costmap_prohibition_layer.cpp
@@ -33,7 +33,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- * Author: Christoph Rösmann
+ * Author: Stephan Kurzawe
  *********************************************************************/
 
 
